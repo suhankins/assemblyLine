38c3b051d2e2a152d5a166d7189ef45046194708
Dimitri Sukhankin <90870844+suhankins@users.noreply.github.com>
2022-05-26 19:27:16 +0300

    Update HistoryCommand.java

--- src/shared/commands/HistoryCommand.java
+++ src/shared/commands/HistoryCommand.java
@@ -2,18 +2,35 @@ package assemblyline.commands;
 
 import assemblyline.utils.IO;
 
+import org.json.JSONObject;
+
+/**
+ * Prints last 12 executed commands
+ */
 public class HistoryCommand extends Command {
-    @Override
-    public void execute(String[] args) {
+    public JSONObject request(String[] args) {
         for (int i = 11; i >= 0; i--) {
             if (history[i] != null) {
                 IO.print("%s%n", history[i]);
             }
         }
+        return null;
     }
 
-    @Override
+    public JSONObject respond(JSONObject args) {
+        for (int i = 11; i >= 0; i--) {
+            if (history[i] != null) {
+                IO.print("%s%n", history[i]);
+            }
+        }
+        return null;
+    }
+
+    public void react(JSONObject args) {
+        return;
+    }
+
     public String getHelp() {
-        return String.format("Prints 12 last successfully executed commands.%n%nUsage: history");
+        return String.format("Prints 12 last executed commands.%n%nUsage: history");
     }
 }
