c77857efcb97d8a7e12c5cc3b09dafa053c228ba
Dimitri Sukhankin <90870844+suhankins@users.noreply.github.com>
2022-05-30 02:02:23 +0300

    Update ShowCommand.java

--- src/shared/commands/ShowCommand.java
+++ src/shared/commands/ShowCommand.java
@@ -5,18 +5,40 @@ import java.util.Enumeration;
 import assemblyline.VehicleCollection;
 import assemblyline.utils.IO;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+/**
+ * Shows all elements of the collection
+ */
 public class ShowCommand extends Command {
-    @Override
-    public void execute(String[] args) {
+    public JSONObject request(String[] args) {
+        return new JSONObject().put("command", "show");
+    }
+
+    public JSONObject respond(JSONObject args) {
         Enumeration keys = VehicleCollection.vehicleCollection.keys();
-        IO.print("List of vehicles:%n");
+        JSONObject data = new JSONObject();
         while (keys.hasMoreElements()) {
             int k = (int)keys.nextElement();
-            IO.print("[%d] %s%n%n", k, VehicleCollection.vehicleCollection.get(k).toString());
+            data.put(Integer.toString(k), String.format("[%d] %s%n%n", k, VehicleCollection.vehicleCollection.get(k).toString()));
+        }
+        return new JSONObject().put("command", "show").put("data", data);
+    }
+
+    public void react(JSONObject args) {
+        JSONObject data = args.getJSONObject("data");
+        JSONArray names = data.names();
+        if (names == null) {
+            IO.print("List is empty%n");
+        } else {
+            IO.print("List of vehicles:%n");
+            for (int i = 0; i < names.length(); i++) {
+                IO.print("%s%n", data.getString(names.getString(i)));
+            }
         }
     }
 
-    @Override
     public String getHelp() {
         return String.format("Prints list of vehicles.%n%nUsage: show");
     }
