46bd520867623b2d9e6d844285db816a9bfe8c2d
suhankins <punishedbernadetta@niuitmo.ru>
2023-03-24 15:51:34 +0400

    compile scripts use correct jar file

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-javac -classpath "json-20211205.jar;." -d . ./src/shared/*.java ./src/shared/Vehicles/*.java ./src/shared/utils/*.java ./src/shared/commands/*.java ./src/server/*.java ./src/client/*.java ./src/client/utils/*.java ./src/server/utils/*.java
+javac -classpath "json.jar;." -d . ./src/shared/*.java ./src/shared/Vehicles/*.java ./src/shared/utils/*.java ./src/shared/commands/*.java ./src/server/*.java ./src/client/*.java ./src/client/utils/*.java ./src/server/utils/*.java
